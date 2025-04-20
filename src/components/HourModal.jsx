96dde895b8043236d99a5427ec94e62e4f7a8754
Davron1244 <davronzokirov187@gmail.com>
2025-04-20 12:12:54 +0500

    new code

--- src/components/HourModal.jsx
+++ src/components/HourModal.jsx
@@ -3,17 +3,19 @@ import { IoMdCloseCircle } from "react-icons/io";
 
 const HourModal = ({ closeHourModal }) => {
   return (
-    <div className="border-hour w-[375px] left-194 absolute top-[200px] bg-[#2C2F35] rounded-2xl text-white shadow-sky-500 p-5">
-      <div className="flex justify-end">
-        <IoMdCloseCircle
-          onClick={closeHourModal}
-          size={30}
-          className="cursor-pointer"
-        />
+    <div className="container">
+      <div className="border-hour w-[375px] ml-[30px] absolute top-[200px] bg-[#2C2F35] rounded-2xl text-white shadow-sky-500 p-5">
+        <div className="flex justify-end">
+          <IoMdCloseCircle
+            onClick={closeHourModal}
+            size={30}
+            className="cursor-pointer"
+          />
+        </div>
+        <img width={400} style={{ marginTop: 40 }} src="/Group 19.svg" alt="" />
+        <img width={400} style={{ marginTop: 10 }} src="/Group 19.svg" alt="" />
+        <img width={400} style={{ marginTop: 10 }} src="/Group 19.svg" alt="" />
       </div>
-      <img width={400} style={{marginTop:40}} src="/Group 19.svg" alt="" />
-      <img width={400} style={{marginTop:10}} src="/Group 19.svg" alt="" />
-      <img width={400} style={{marginTop:10}} src="/Group 19.svg" alt="" />
     </div>
   );
 };
